3d27878ae4cd9da443bd14d38e014fdcb0ae7a7a
paskimeo <paskimeo@gmail.com>
2022-06-23 16:04:29 +0200

    aggiunti metodi esercizio base e main per test

--- src/calcolatrice/CalcoliHelper.java
+++ src/calcolatrice/CalcoliHelper.java
@@ -8,12 +8,56 @@ public class CalcoliHelper
 		
 	}
 	
-	public static int somma(int a, int b) {
+	//somma
+	public static int somma(int a, int b) 
+	{
 
 		return a+b;
 	}
 	
+	// sottrazione
+	public static int differenza(int a, int b) 
+	{
+
+		return a-b;
+	}
 	
+	//moltiplicazione
+	public static int moltiplicazione(int a, int b) 
+	{
+
+		return a*b;
+	}
 	
+	//valore assoluto
+	public static int valoreAssoluto(int a) 
+	{
+		if(a<0) {
+			return -a;
+		}else {
+			return a;
+		}
+		
+	}
 	
+	//valore minore tra 2 valore
+	public static int valoreMinore(int a, int b) 
+	{
+		if(a<b) {
+			return a;
+		}else {
+			return b;
+		}
+		
+	}
+	//valore maggiore tra 2 valore
+	public static int valoreMaggiore(int a, int b) 
+	{
+		if(a>b) {
+			return a;
+		}else {
+			return b;
+		}
+		
+	}
 }
---
+++ src/calcolatrice/CalcoliTest.java
@@ -0,0 +1,21 @@
+package calcolatrice;
+
+public class CalcoliTest {
+
+	public static void main(String[] args) {
+	
+
+		  System.out.println(CalcoliHelper.somma(2, 2));
+		  
+		  System.out.println(CalcoliHelper.differenza(2, 2));
+		  
+		  System.out.println(CalcoliHelper.moltiplicazione(2, 2));
+		  
+		  System.out.println(CalcoliHelper.valoreAssoluto(-2));
+		  
+		  System.out.println(CalcoliHelper.valoreMaggiore(5, 6));
+		  
+		  System.out.println(CalcoliHelper.valoreMinore(6, 5));
+	}
+
+}
